9e0595e69a812be95998a3181af6e5f4c4e2d713
Muhammad Luthfi Khusyasy <khusyasy@gmail.com>
2021-06-22 08:12:25 +0700

    set navbar height

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -2,7 +2,7 @@ import { Navbar, Container, Nav, Button } from "react-bootstrap";
 
 function TopBar() {
   return (
-  <Navbar bg="dark" variant="dark" expand="lg">
+  <Navbar bg="dark" variant="dark" expand="lg" fixed="top" style={styles.navbar}>
     <Container>
       <Navbar.Brand href="/">FlameChat</Navbar.Brand>
       <Nav>
@@ -11,6 +11,12 @@ function TopBar() {
     </Container>
   </Navbar>
   )
+}
+
+const styles = {
+  navbar: {
+    height: "4rem",
+  }
 }
 
 export default TopBar;
