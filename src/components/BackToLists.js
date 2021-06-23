05f84a32c59d7968c0db2d3988e4563a1cd8476f
Muhammad Luthfi Khusyasy <khusyasy@gmail.com>
2021-06-23 08:08:24 +0700

    fix svd property on jsx

--- src/components/BackToLists.js
+++ src/components/BackToLists.js
@@ -8,7 +8,7 @@ function BackToLists() {
   return send && (
     <Button variant="dark" onClick={()=>setSend("")}>
       <svg xmlns="http://www.w3.org/2000/svg" style={styles.icon} viewBox="0 0 20 20" fill="currentColor">
-        <path fill-rule="evenodd" d="M9.707 14.707a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 1.414L7.414 9H15a1 1 0 110 2H7.414l2.293 2.293a1 1 0 010 1.414z" clip-rule="evenodd" />
+        <path fillRule="evenodd" d="M9.707 14.707a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 1.414L7.414 9H15a1 1 0 110 2H7.414l2.293 2.293a1 1 0 010 1.414z" clipRule="evenodd" />
       </svg>
     </Button>
   )
